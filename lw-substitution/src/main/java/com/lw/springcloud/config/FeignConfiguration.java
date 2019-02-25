c9e3cb84d42875bbbbca6602adefdd96ba85e6ac
wei.liu <wei.liu@qiaofang.info>
2019-02-25 14:17:21 +0800

    feign配置以及定义feign认证Interceptor

--- lw-substitution/src/main/java/com/lw/springcloud/config/FeignConfiguration.java
+++ lw-substitution/src/main/java/com/lw/springcloud/config/FeignConfiguration.java
@@ -1,6 +1,8 @@
 package com.lw.springcloud.config;
 
+import com.lw.springcloud.interceptor.BasicAuthInterceptor;
 import feign.Logger;
+import feign.Request;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,5 +16,19 @@ public class FeignConfiguration {
     @Bean
     Logger.Level feignLoggerLevel(){
         return Logger.Level.FULL;
+    }
+
+    @Bean
+    BasicAuthInterceptor BasicAuthInterceptor(){
+        return new BasicAuthInterceptor("123");
+    }
+
+    /**
+     * 配置feign 连接超时时间 读取超时时间
+     * @return
+     */
+    @Bean
+    Request.Options options(){
+        return new Request.Options(5*1000,10*1000);
     }
 }
