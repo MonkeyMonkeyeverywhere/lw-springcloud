d44dda3afd313e7ed5f64ccbf270252bdfc72dce
wei.liu <wei.liu@qiaofang.info>
2019-03-07 18:47:29 +0800

    添加sleuth依赖

--- lw-house-service/src/main/java/com/lw/springcloud/demo/controller/HouseController.java
+++ lw-house-service/src/main/java/com/lw/springcloud/demo/controller/HouseController.java
@@ -1,5 +1,7 @@
 package com.lw.springcloud.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/house")
 public class HouseController {
+
+    private Logger logger = LoggerFactory.getLogger(HouseController.class);
 
     @Value("${server.port}")
     private String serverPort;
@@ -16,6 +20,7 @@ public class HouseController {
 
     @RequestMapping("/hello")
     public String hello(){
+        logger.info("request hello");
         return defaultZone+"hello from lw-house-service!"+serverPort;
     }
 }
